9a3f27c32556cbefda7ac178aa9d62d848dffa55
Spykerwolf <willsmal.nz@gmail.com>
2022-10-11 18:12:32 +1300

    Added loop every 5 min

--- qBit.py
+++ qBit.py
@@ -1,26 +1,34 @@
+
+
 import requests
 import jq
+import time
 
-#Errored torrents
-urlErrored = 'http://192.168.1.224:8080/api/v2/torrents/info?filter=errored&category=test'
-hashesErrored = (jq.compile(".[] | .hash").input(requests.get(urlErrored).json()).text()).replace('"', '').split()
-namesErrored = (jq.compile(".[] | .name").input(requests.get(urlErrored).json()).text()).replace('"', '')
+repeat = 1
+while repeat == 1:
+    #Errored torrents
+    urlErrored = 'http://192.168.1.224:8080/api/v2/torrents/info?filter=errored&category=test'
+    hashesErrored = (jq.compile(".[] | .hash").input(requests.get(urlErrored).json()).text()).replace('"', '').split()
+    namesErrored = (jq.compile(".[] | .name").input(requests.get(urlErrored).json()).text()).replace('"', '')
 
-deleteErrored = ("http://192.168.1.224:8080/api/v2/torrents/delete?hashes=" + "|".join(hashesErrored) + "&deleteFiles=true")
+    deleteErrored = ("http://192.168.1.224:8080/api/v2/torrents/delete?hashes=" + "|".join(hashesErrored) + "&deleteFiles=true")
 
-if namesErrored != "":
-    print("Deleted the following Errored torrents:")
-    print(namesErrored)
-    requests.get(deleteErrored)
+    if namesErrored != "":
+        print("Deleted the following Errored torrents:")
+        print(namesErrored)
+        requests.get(deleteErrored)
+        print("")
 
-#Completed torrents
-urlCompleted = 'http://192.168.1.224:8080/api/v2/torrents/info?filter=completed'
-hashesCompleted = (jq.compile(".[] | .hash").input(requests.get(urlCompleted).json()).text()).replace('"', '').split()
-namesCompleted = (jq.compile(".[] | .name").input(requests.get(urlCompleted).json()).text()).replace('"', '')
+    #Completed torrents
+    urlCompleted = 'http://192.168.1.224:8080/api/v2/torrents/info?filter=completed'
+    hashesCompleted = (jq.compile(".[] | .hash").input(requests.get(urlCompleted).json()).text()).replace('"', '').split()
+    namesCompleted = (jq.compile(".[] | .name").input(requests.get(urlCompleted).json()).text()).replace('"', '')
 
-deleteCompleted = ("http://192.168.1.224:8080/api/v2/torrents/delete?hashes=" + "|".join(hashesCompleted) + "&deleteFiles=false")
+    deleteCompleted = ("http://192.168.1.224:8080/api/v2/torrents/delete?hashes=" + "|".join(hashesCompleted) + "&deleteFiles=false")
 
-if namesCompleted != "":
-    print("Deleted the following Completed torrents:")
-    print(namesCompleted)
-    requests.get(deleteCompleted)
+    if namesCompleted != "":
+        print("Deleted the following Completed torrents:")
+        print(namesCompleted)
+        requests.get(deleteCompleted)
+        print("")
+    time.sleep(300)
