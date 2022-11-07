0c93f4d60a010f7925eddb56f939d7357f13ff8a
Spykerwolf <willsmal.nz@gmail.com>
2022-11-08 10:35:43 +1300

    Deleting working

--- qBit.py
+++ qBit.py
@@ -1,34 +1,19 @@
-
-
-import requests
-import jq
+from qbittorrent import Client
+qb = Client('http://localhost:8080')
+qb.login()
 import time
 
-repeat = 1
-while repeat == 1:
-    #Errored torrents
-    urlErrored = 'http://192.168.1.224:8080/api/v2/torrents/info?filter=errored'
-    hashesErrored = (jq.compile(".[] | .hash").input(requests.get(urlErrored).json()).text()).replace('"', '').split()
-    namesErrored = (jq.compile(".[] | .name").input(requests.get(urlErrored).json()).text()).replace('"', '')
+while True:
 
-    deleteErrored = ("http://192.168.1.224:8080/api/v2/torrents/delete?hashes=" + "|".join(hashesErrored) + "&deleteFiles=true")
+    hashes = []
+    completedTorrents = qb.torrents(filter='completed', category='RARBG')
 
-    if namesErrored != "":
-        print("Deleted the following Errored torrents:")
-        print(namesErrored)
-        requests.get(deleteErrored)
-        print("")
-
-    #Completed torrents
-    urlCompleted = 'http://192.168.1.224:8080/api/v2/torrents/info?filter=completed'
-    hashesCompleted = (jq.compile(".[] | .hash").input(requests.get(urlCompleted).json()).text()).replace('"', '').split()
-    namesCompleted = (jq.compile(".[] | .name").input(requests.get(urlCompleted).json()).text()).replace('"', '')
-
-    deleteCompleted = ("http://192.168.1.224:8080/api/v2/torrents/delete?hashes=" + "|".join(hashesCompleted) + "&deleteFiles=false")
-
-    if namesCompleted != "":
-        print("Deleted the following Completed torrents:")
-        print(namesCompleted)
-        requests.get(deleteCompleted)
-        print("")
+    if completedTorrents:
+        for torrent in completedTorrents:
+            hashes.append(torrent['hash'])
+        print("Deleting the following torrents: \n" + torrent['name'])
+        qb.delete(hashes)
+    else:
+        print("Nothing more to delete, waiting...")
+    
     time.sleep(300)
